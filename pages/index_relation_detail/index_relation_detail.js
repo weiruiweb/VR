e597aa1c4fecfe3367cee497bfd6cfe38e283a0b
weiruiweb <18192654258@163.com>
2018-11-09 20:31:36 +0800

    end

--- pages/index_relation_detail/index_relation_detail.js
+++ pages/index_relation_detail/index_relation_detail.js
@@ -8,16 +8,37 @@ Page({
     currentId:0,
   },
   //事件处理函数
-  tab(e){
-   this.setData({
-      currentId:e.currentTarget.dataset.id
-    })
-  },
-  onLoad(options) {
+
+   onLoad(options){
     const self = this;
-    self.setData({
-      img:app.globalData.img
-    })
+    self.data.id = options.id;
+    self.getMainData();
+   
+  },
+
+  getMainData(){
+    const self = this;
+  
+    const postData = {};
+ 
+    postData.searchItem = {
+      id:self.data.id,
+      thirdapp_id:getApp().globalData.thirdapp_id,
+    }
+  
+    const callback = (res)=>{
+      if(res.info.data.length>0){
+        self.data.mainData = res.info.data[0];
+        self.data.mainData.content = api.wxParseReturn(res.info.data[0].content).nodes;
+      }else{
+        self.data.isLoadAll = true;
+        api.showToast('数据错误','none');
+      };
+      self.setData({
+        web_mainData:self.data.mainData,
+      });  
+    };
+    api.articleGet(postData,callback);
   },
   intoPath(e){
     const self = this;
